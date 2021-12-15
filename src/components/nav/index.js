5fccf3fed58079232120804b2d75ec57eca02de7
yulongkelly <1019917158wu@gmail.com>
2021-12-14 21:54:09 -0500

    use map

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -4,6 +4,7 @@ import { Link as LinkR } from "react-router-dom";
 import { NavbarContainer, NavManu, NavItem, NavLink, Img } from "./style";
 
 const Navbar = () => {
+  const sections = ["home", "about", "projects"];
   return (
     <>
       <NavbarContainer>
@@ -14,15 +15,40 @@ const Navbar = () => {
           />
         </LinkR>
         <NavManu>
-          <NavItem>
-            <NavLink to='home' activeClass='active' smooth={true} spy={true}>Home</NavLink>
+          {sections.map((section) => {
+            return (
+              <NavItem>
+                <NavLink
+                  to={section}
+                  activeClass="active"
+                  smooth={true}
+                  spy={true}
+                >
+                  {section[0].toUpperCase() + section.substring(1)}
+                </NavLink>
+              </NavItem>
+            );
+          })}
+          {/* <NavItem>
+            <NavLink to="home" activeClass="active" smooth={true} spy={true}>
+              Home
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink to='about' activeClass='active' smooth={true} spy={true}>About</NavLink>
+            <NavLink to="about" activeClass="active" smooth={true} spy={true}>
+              About
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink to='projects' activeClass='active' smooth={true} spy={true}>Projects</NavLink>
-          </NavItem>
+            <NavLink
+              to="projects"
+              activeClass="active"
+              smooth={true}
+              spy={true}
+            >
+              Projects
+            </NavLink>
+          </NavItem> */}
         </NavManu>
       </NavbarContainer>
     </>
